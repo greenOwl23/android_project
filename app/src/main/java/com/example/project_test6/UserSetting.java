e635a7295138eff89a97090fa2553ba5dfd41695
patrickstrauss <patcharagitstrauss@gmail.com>
2019-09-24 23:06:04 +0700

    user setting main page

--- app/src/main/java/com/example/project_test6/UserSetting.java
+++ app/src/main/java/com/example/project_test6/UserSetting.java
@@ -1,6 +1,7 @@
 package com.example.project_test6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,29 +9,52 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.project_test6.ui.profile.ProfileFragment;
+
 public class UserSetting extends AppCompatActivity {
 
-    private Button button;
+    private Button button_logout;
+    private Button button_user;
+    private Button button_balance;
+    private Button button_budget;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_setting);
 
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-
-        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.5));
-
-        button = findViewById(R.id.logout);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //Logout user
+        button_user = findViewById(R.id.user_Account_Setting);
+        button_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 startActivity(new Intent(UserSetting.this, MainPage.class));
             }
         });
+
+        button_balance = findViewById(R.id.account_balance_Setting);
+        button_balance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserSetting.this, MainPage.class));
+            }
+        });
+
+        button_budget = findViewById(R.id.budget_setting);
+        button_budget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserSetting.this, MainPage.class));
+            }
+        });
+
+        button_logout = findViewById(R.id.logout);
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Logout user
+
+            }
+        });
+
 
     }
 }
